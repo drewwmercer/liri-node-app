12d4f8295a9aba61b964ccad94ee4ac49611bfdb
Drew Mercer <japanexpress@gmail.com>
2017-08-22 20:54:48 -0400

    Including twitter app keys

--- js/keys.js
+++ js/keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '4B1UNSVuxTga0rI28VpSqjb90',
+  consumer_secret: 'GdbCSQV3vjnqPqfUqKEWlyzCkUrTcaMmj6uyCcfJH8UHk0k0ug',
+  access_token_key: '900124746003742720-yMeq3gFCXVYrdeDq9qYRLnmAFmHYVU2',
+  access_token_secret: 'MeVBI49wGQ8go36nafgP7CwpdJoMJ0seyJf31ImDoImSr',
 }
