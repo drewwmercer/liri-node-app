e093eb9f7c7e04201e16b5d91d7d5fc0c20af02c
Drew Mercer <japanexpress@gmail.com>
2017-08-22 20:45:01 -0400

    Adding placeholders for keys

---
+++ js/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
