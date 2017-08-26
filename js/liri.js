823a5cb52d46b8d3948c9717201b6449987c3380
Drew Mercer <japanexpress@gmail.com>
2017-08-26 12:30:37 -0400

    Updating liri logic

--- js/liri.js
+++ js/liri.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var request = require("request");
-var keys = require("keys.js");
+var keys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require ("spotify");
 
