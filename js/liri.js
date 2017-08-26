56f2d462815f7374e18d1b326de63de6841fb3e1
Drew Mercer <japanexpress@gmail.com>
2017-08-26 12:35:04 -0400

    Updating liri logic

--- js/liri.js
+++ js/liri.js
@@ -7,6 +7,20 @@ var spotify = require ("spotify");
 var liriOption = process.argv[2];
 var searchTerm = process.argv[3];
 
+switch(liriOption) {
+    case "my-tweets": myTweets(); break;
+    case "spotify-this-song": spotifyThisSong(); break;
+    case "movie-this": movieThis(); break;
+    case "do-what-it-says": doWhatItSays(); break;
+    default: console.log("\r\n" +"Type one of the following commands after 'node liri.js' : " +"\r\n"+
+        "1. my-tweets 'any twitter name' " +"\r\n"+
+        "2. spotify-this-song 'any song name' "+"\r\n"+
+        "3. movie-this 'any movie name' "+"\r\n"+
+        "4. do-what-it-says."+"\r\n");
+        //"Be sure to put the movie or song name in quotation marks if it's more than one word.");
+};
+
+
 // Then run a request to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=40e9cece";
 
