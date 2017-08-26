acffb7d9258ea81aa8257cf292654b397b2b2b27
Drew Mercer <japanexpress@gmail.com>
2017-08-26 12:28:52 -0400

    Updating logic and editng package

--- js/liri.js
+++ js/liri.js
@@ -3,3 +3,23 @@ var request = require("request");
 var keys = require("keys.js");
 var twitter = require("twitter");
 var spotify = require ("spotify");
+
+var liriOption = process.argv[2];
+var searchTerm = process.argv[3];
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=40e9cece";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
