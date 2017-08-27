a3a9746ce2802638c6244ba8581912e9e2a0febe
Drew Mercer <japanexpress@gmail.com>
2017-08-26 22:23:18 -0400

    Updating liri with twitter request

--- js/liri.js
+++ js/liri.js
@@ -84,14 +84,4 @@ function movieThis() {
   });
 }
 
-// Then run a request to the OMDB API with the movie specified
-
-// This line is just to help us debug against the actual URL.
-
-// request(queryUrl, function(error, response, body) {
-//   // If the request is successful
-//     // Parse the body of the site and recover just the imdbRating
-//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     console.log("Release Year: " + JSON.parse(body).Year);
-//   }
-// });
+function myTweets() {}
