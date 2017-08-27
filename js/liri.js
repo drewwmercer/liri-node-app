72ae02a839f1e49af9434b4d295605698844801e
Drew Mercer <japanexpress@gmail.com>
2017-08-27 00:50:21 -0400

    Messing with twitter API

--- js/liri.js
+++ js/liri.js
@@ -84,7 +84,16 @@ function movieThis() {
 }
 
 function myTweets() {
-    var twitOption ;
+  var twitOption = new twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+  });
+  var handle = searchTerm;
+  var twitterPassHandle = "screen_name: " + handle;
+  console.log(twitterPassHandle); //testing that this is working so far
+  twitOption.get;
 }
 
 function spotifyThisSong() {}
